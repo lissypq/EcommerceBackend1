e7083ef005c50c248924242d9cd9c64a395ec3e7
lissypq <lissy0147@hotmail.com>
2025-06-11 21:30:53 -0400

    Add files via upload

--- src/domain/entities/OrderDetails.js
+++ src/domain/entities/OrderDetails.js
@@ -1,12 +1,11 @@
 class OrderDetails {
-  constructor({ id,orderid,productid, quantity, unitprice,createdate, updatedate }) {
+  constructor({ id,orderid,productid, quantity, unitprice}) {
     this.id = id; 
     this.orderid = orderid;
     this.productid = productid;
     this.quantity = quantity;
     this.unitprice = unitprice;
-    this.createdate = createdate;
-    this.updatedate = updatedate;
+   
    }
 }
  
