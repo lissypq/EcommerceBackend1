d3c78ca3d047688f32730778776e936b9627a463
lissypq <lissy0147@hotmail.com>
2025-06-19 00:15:21 -0400

    Update userRoutes.js

--- src/adapters/routes/userRoutes.js
+++ src/adapters/routes/userRoutes.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const { Router }     = require('express');
-const UserController = require('../controllers/UserController');
- 
-module.exports = (signUpUseCase) => {
-  const router = Router();
-  const controller = new UserController(signUpUseCase);
- 
-  // POST /api/v1/users
-  router.post('/', controller.signUp.bind(controller));
- 
-  return router;
-=======
 const { Router }     = require('express');
 const UserController = require('../controllers/UserController');
  
