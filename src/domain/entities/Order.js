58502415be00a7a4ceaf1716d10788f5fdc2ad44
lissypq <lissy0147@hotmail.com>
2025-06-19 00:19:07 -0400

    Update Order.js

--- src/domain/entities/Order.js
+++ src/domain/entities/Order.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-class Order {
-  constructor({ id, userid,totalprice,orderdate,orderstatus}) {
-    this.id = id;
-    this.userid = userid;
-    this.totalprice = totalprice;
-    this.orderdate = orderdate;
-    this.orderstatus = orderstatus; 
-   }
-}
- 
-=======
 class Order {
   constructor({ customerid,totalprice,orderdate,orderstatus}) {
     this.customerid = customerid;
@@ -18,6 +6,5 @@ class Order {
     this.orderstatus = orderstatus; 
    }
 }
- 
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = Order;
