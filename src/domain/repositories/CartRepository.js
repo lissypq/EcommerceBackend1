0ba4f69ac6c0f026f5d40e1851da264f82440f7e
lissypq <lissy0147@hotmail.com>
2025-06-19 00:19:56 -0400

    Update CartRepository.js

--- src/domain/repositories/CartRepository.js
+++ src/domain/repositories/CartRepository.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 class CartRepository {
 async getAll() { throw new Error('Method not implemented'); }
   async getById(id) { throw new Error('Method not implemented'); }
   async create(cart) { throw new Error('Method not implemented'); }
 }
  
-=======
-class CartRepository {
-async getAll() { throw new Error('Method not implemented'); }
-  async getById(id) { throw new Error('Method not implemented'); }
-  async create(cart) { throw new Error('Method not implemented'); }
-}
- 
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = CartRepository;
