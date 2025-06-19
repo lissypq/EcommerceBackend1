f4cb6f92f0ca5c08afa68f00970a60ee9a999f11
lissypq <lissy0147@hotmail.com>
2025-06-19 00:26:54 -0400

    Update MongoOrderRepository.js

--- src/infraestructure/repositories/MongoOrderRepository.js
+++ src/infraestructure/repositories/MongoOrderRepository.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-const OrderRepository = require('../../domain/repositories/OrderRepository');
-const OrderModel = require('../database/models/OrderModel');
-const Order = require('../../domain/entities/Order');
-
-class MongoOrderRepository extends MongoOrderRepository {
-  async getAll() {
-    const order = await OrderModel.find();
-    return order.map(o => new Order(o.toObject()));
-  }
-
-  async create(order) {
-    const newOrder = await OrderModel.create(order);
-    return new Order(newOrder.toObject());
-  }
-
-
-
-  async getById(id) {
-    const order = await OrderModel.findById(id);
-    if (!order) {
-      return null;
-    }
-    return new Order(order.toObject());
-    }
-    
- 
-  }
-=======
 const OrderRepository = require('../../domain/repositories/OrderRepository');
 const OrderModel = require('../database/models/OrderModel');
 const Order = require('../../domain/entities/Order');
@@ -54,5 +25,5 @@ class MongoOrderRepository extends OrderRepository {
     
  
   }
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = MongoOrderRepository;
