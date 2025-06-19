bfaedad6334f2bf01ec2b48705f84540bc14def3
lissypq <lissy0147@hotmail.com>
2025-06-19 00:14:23 -0400

    Update orderdetailRoutes.js

--- src/adapters/routes/orderdetailRoutes.js
+++ src/adapters/routes/orderdetailRoutes.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-
-module.exports = (OrderDetailController) => {
-  /**
- * @swagger
- * tags:
- *   name: Products
- *   description: Gestión de productos
- */
-
-/**
- * @swagger
- * /api/products:
- *   get:
- *     summary: Obtiene todos los productos
- *     tags: [Products]
- *     responses:
- *       200:
- *         description: Lista de productos
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Product'
- */
-  router.get('/', (req, res) => OrderDetailController.getAll(req, res));
-  router.post('/', (req, res) => OrderDetailController.create(req, res));
-
-  return router;
-=======
 const express = require('express');
 const router = express.Router();
 
