fb8df05be348890b4c6885059e1716002d0dc801
lissypq <lissy0147@hotmail.com>
2025-06-19 00:19:27 -0400

    Update OrderDetails.js

--- src/domain/entities/OrderDetails.js
+++ src/domain/entities/OrderDetails.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-class OrderDetails {
-  constructor({ id,orderid,productid, quantity, unitprice}) {
-    this.id = id; 
-    this.orderid = orderid;
-    this.productid = productid;
-    this.quantity = quantity;
-    this.unitprice = unitprice;
-   
-   }
-}
- 
-=======
 class OrderDetails {
   constructor({ orderid,productid, quantity, unitprice}) {
      this.orderid = orderid;
@@ -21,5 +8,5 @@ class OrderDetails {
    }
 }
  
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = OrderDetails;
