999f80edd53f9cb1e0c233357e9ebcdc63c73611
lissypq <lissy0147@hotmail.com>
2025-06-19 00:18:40 -0400

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config();
 
 module.exports = {
@@ -6,13 +5,5 @@ module.exports = {
   mongoUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   DB_TYPE: process.env.DB_TYPE,
-=======
-require('dotenv').config();
 
-module.exports = {
-  port: process.env.PORT || 5000,
-  mongoUri: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET,
-  DB_TYPE: process.env.DB_TYPE,
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
 };
