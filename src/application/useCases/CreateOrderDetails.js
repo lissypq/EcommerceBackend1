7f7dd541a02435698f7eb8fdb063c78f20be4d59
lissypq <lissy0147@hotmail.com>
2025-06-19 00:17:09 -0400

    Update CreateOrderDetails.js

--- src/application/useCases/CreateOrderDetails.js
+++ src/application/useCases/CreateOrderDetails.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-const orderdetails = require('../../domain/entities/OrderDetails');
- 
-class CreateOrderdetails {
-  constructor(orderDetailsRepository) {
-    this.orderDetailsRepository = orderDetailsRepository;
-  }
- 
-  async execute(orderData) {
-    const orderdetails = new orderdetails(orderData);
-    return await this.orderDetailsRepository.create(orderdetails);
-  }
-}
- 
-=======
 const Orderdetails = require('../../domain/entities/OrderDetails');
  
 class CreateOrderdetails {
@@ -26,5 +11,5 @@ class CreateOrderdetails {
   }
 }
  
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = CreateOrderdetails;
