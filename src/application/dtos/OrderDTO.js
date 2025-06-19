724fdd8530c2a2a2f61d19264ef1a8d2f03c5650
lissypq <lissy0147@hotmail.com>
2025-06-19 00:16:13 -0400

    Update OrderDTO.js

--- src/application/dtos/OrderDTO.js
+++ src/application/dtos/OrderDTO.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-class OrderDTO{
-    constructor(order) {
-        this.id = order.id;
-        this.userid = order.userid;
-        this.totalprice = order.totalprice;
-        this.orderdate = order.orderdate;
-        this.orderstatus = order.orderstatus;
-        }
-}
-
-=======
 class OrderDTO{
     constructor(order) {
         this.id = order._id;
@@ -19,6 +7,4 @@ class OrderDTO{
         this.orderstatus = order.orderstatus;
         }
 }
-
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
 module.exports = OrderDTO;
