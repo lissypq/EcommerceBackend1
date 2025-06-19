7f92197b6278d9e91eff3acc1568b49d16066228
lissypq <lissy0147@hotmail.com>
2025-06-19 00:20:30 -0400

    Update OrderRepository.js

--- src/domain/repositories/OrderRepository.js
+++ src/domain/repositories/OrderRepository.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-class OrderRepository {
-  async getAll() { throw new Error('Method not implemented'); }
-  async getById(id) { throw new Error('Method not implemented'); }
-  async create(order) { throw new Error('Method not implemented'); }
-  async update(id) { throw new Error('Method not implemented'); }
-  async delete(id) { throw new Error('Method not implemented'); }
-}
- 
-=======
 class OrderRepository {
   async getAll() { throw new Error('Method not implemented'); }
   async getById(id) { throw new Error('Method not implemented'); }
   async create(order) { throw new Error('Method not implemented'); }
 
 }
- 
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = OrderRepository;
