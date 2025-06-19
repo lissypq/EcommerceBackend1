dbc9b0ecb2988f7d5955954f23f2a0e3e53dc80d
lissypq <lissy0147@hotmail.com>
2025-06-19 00:16:31 -0400

    Update OrderDetailsDTO.js

--- src/application/dtos/OrderDetailsDTO.js
+++ src/application/dtos/OrderDetailsDTO.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-class OrderDetailsDTO{
-constructor(orderdetails) {
-    this.id = orderdetails.id;
-    this.orderid = orderdetails.orderid;
-    this.productid = orderdetails.productid;
-    this.quantity = orderdetails.quantity;
-    this.unitprice = orderdetails.unitprice;
-    }        
-}
-
-=======
 class OrderDetailsDTO{
 constructor(orderdetails) {
     this.id = orderdetails._id;
@@ -20,5 +8,5 @@ constructor(orderdetails) {
     }        
 }
 
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports =OrderDetailsDTO;
