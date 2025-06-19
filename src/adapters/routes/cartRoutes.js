811c6cd532451cf898b0ee25a1d6db0a866e3a9c
lissypq <lissy0147@hotmail.com>
2025-06-19 00:13:15 -0400

    Update cartRoutes.js

--- src/adapters/routes/cartRoutes.js
+++ src/adapters/routes/cartRoutes.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-
-module.exports = (CartController) => {
-  /**
- * @swagger
- * tags:
- *   name: Products
- *   description: Gestión de productos
- */
-
-/**
- * @swagger
- * /api/products:
- *   get:
- *     summary: Obtiene todos los productos
- *     tags: [Products]
- *     responses:
- *       200:
- *         description: Lista de productos
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Product'
- */
-  router.get('/', (req, res) => CartController.getAll(req, res));
-  router.post('/', (req, res) => CartController.create(req, res));
-
-  return router;
-=======
 const express = require('express');
 const router = express.Router();
 
@@ -62,5 +29,5 @@ module.exports = (CartController) => {
   router.post('/', (req, res) => CartController.create(req, res));
 
   return router;
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 };
