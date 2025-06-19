135025ecc72f449907415fd0bd401355170f722f
lissypq <lissy0147@hotmail.com>
2025-06-19 00:21:43 -0400

    Delete src/infraestructure/database/model directory

--- src/infraestructure/database/model/User.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },      // passwordHashed
-  roles:    { type: [String], default: ['user'] }
-}, { timestamps: true });
-
-module.exports = model('User', userSchema);
