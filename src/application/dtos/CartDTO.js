29e1e3cab6bc924274c94a2dd59a00e6201322e9
lissypq <lissy0147@hotmail.com>
2025-06-19 00:15:44 -0400

    Update CartDTO.js

--- src/application/dtos/CartDTO.js
+++ src/application/dtos/CartDTO.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-class CartDTO{
-constructor(Cart) {
-    this.id = Cart.id;
-    this.items = Cart.items || [];
-    this.totalprice = Cart.totalprice || 0;
-    this.userid = Cart.userid || null;
-    }        
-}
-
-=======
 class CartDTO{
 constructor(Cart) {
     this.id = Cart._id;
@@ -18,5 +7,5 @@ constructor(Cart) {
     }        
 }
 
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports =CartDTO;
