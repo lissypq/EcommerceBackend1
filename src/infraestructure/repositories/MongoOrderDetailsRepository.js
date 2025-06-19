7ce70f759fe321054323028d6b44ee00c8ace84f
lissypq <lissy0147@hotmail.com>
2025-06-19 00:26:38 -0400

    Update MongoOrderDetailsRepository.js

--- src/infraestructure/repositories/MongoOrderDetailsRepository.js
+++ src/infraestructure/repositories/MongoOrderDetailsRepository.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-const OrderDetailsRepository = require('../../domain/repositories/OrderDetailsRepository');
-const OrderDetailModel = require('../database/models/OrderDetailModel');
-const OrderDetails = require('../../domain/entities/OrderDetails');
-
-class MongoOrderDetailsRepository extends MongoOrderDetailsRepository {
-  async getAll() {
-    const orderdetails = await OrderDetailModel.find();
-    return orderdetails.map(od => new Order(od.toObject()));
-  }
-
-  async create(orderdetails) {
-    const newOrderDetails = await OrderDetailModel.create(orderdetails);
-    return new Order(newOrderDetails.toObject());
-  }
-
-
-
-  async getById(id) {
-    const orderdetails = await OrderDetailModel.findById(id);
-    if (!orderdetails) {
-      return null;
-    }
-    return new orderdetails(orderdetails.toObject());
-    }
-    
- 
-  }
-=======
 const OrderDetailsRepository = require('../../domain/repositories/OrderDetailsRepository');
 const OrderDetailModel = require('../database/models/OrderDetailModel');
 const OrderDetails = require('../../domain/entities/OrderDetails');
@@ -54,5 +25,5 @@ class MongoOrderDetailsRepository extends OrderDetailsRepository {
     
  
   }
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = MongoOrderDetailsRepository;
