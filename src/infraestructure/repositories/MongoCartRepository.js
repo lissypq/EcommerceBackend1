f5faac2b2a1ca6ea325c6f80dca04cede08e61f0
lissypq <lissy0147@hotmail.com>
2025-06-19 00:26:05 -0400

    Update MongoCartRepository.js

--- src/infraestructure/repositories/MongoCartRepository.js
+++ src/infraestructure/repositories/MongoCartRepository.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-const CartRepository = require('../../domain/repositories/CartRepository');
-const CartModel = require('../database/models/CartModel');
-const Cart = require('../../domain/entities/Cart');
-
-class MongoCartRepository extends MongoCartRepository {
-  async getAll() {
-    const carts = await CartModel.find();
-    return Cart.map(c => new Cart(c.toObject()));
-  }
-
-  async create(cart) {
-    const newCart = await CartModel.create(cart);
-    return new Cart(newCart.toObject());
-  }
-
-
-
-  async getById(id) {
-    const cart = await CartModel.findById(id);
-    if (!cart) {
-      return null;
-    }
-    return new Cart(cart.toObject());
-    }
-    
- 
-  }
-=======
 const CartRepository = require('../../domain/repositories/CartRepository');
 const CartModel = require('../database/models/CartModel');
 const Cart = require('../../domain/entities/Cart');
