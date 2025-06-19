e82465089159efd31ba44e0d51da57fbe6053984
lissypq <lissy0147@hotmail.com>
2025-06-19 00:09:32 -0400

    Update AuthController.js

--- src/adapters/controllers/AuthController.js
+++ src/adapters/controllers/AuthController.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-class AuthController {
-  constructor(signInUseCase) {
-    this.signInUseCase = signInUseCase;
-  }
- 
-  async signIn(req, res, next) {
-    try {
-      const { username, password } = req.body;
-      const { user, token } = await this.signInUseCase.execute({ username, password });
-      delete user.password;
-      res.json({ user, token });
-    } catch (err) {
-      res.status(401).json({ message: err.message });
-    }
-  }
-}
- 
-module.exports = AuthController;
-=======
 class AuthController {
   constructor(signInUseCase) {
     this.signInUseCase = signInUseCase;
@@ -36,4 +16,3 @@ class AuthController {
 }
 
 module.exports = AuthController;
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
