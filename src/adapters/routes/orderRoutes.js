a237e44696fcb65bdc7e1d61240a7a3cbe348ffd
lissypq <lissy0147@hotmail.com>
2025-06-19 00:14:04 -0400

    Update orderRoutes.js

--- src/adapters/routes/orderRoutes.js
+++ src/adapters/routes/orderRoutes.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-
-module.exports = (OrderController) => {
-  /**
- * @swagger
- * tags:
- *   name: Orders
- *   description: Gestión de pedidos
- */
-
-/**
- * @swagger
- * /api/products:
- *   get:
- *     summary: Obtiene todos los pedidos
- *     tags: [Pedidos]
- *     responses:
- *       200:
- *         description: Lista de pedidos
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/order'
- */
-  router.get('/', (req, res) => OrderController.getAll(req, res));
-  router.post('/', (req, res) => OrderController.create(req, res));
-
-  return router;
-=======
 const express = require('express');
 const router = express.Router();
 
@@ -62,5 +29,5 @@ module.exports = (OrderController) => {
   router.post('/', (req, res) => OrderController.create(req, res));
 
   return router;
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 };
