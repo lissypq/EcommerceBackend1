709f22770e83958bf47129b96a226f5f5452290e
lissypq <lissy0147@hotmail.com>
2025-06-19 00:24:31 -0400

    Update mongoose.js

--- src/infraestructure/database/mongoose.js
+++ src/infraestructure/database/mongoose.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const config = require('../../config');
 
@@ -7,14 +6,5 @@ mongoose.connect(config.mongoUri, {
   useUnifiedTopology: true,
 });
 
-=======
-const mongoose = require('mongoose');
-const config = require('../../config');
 
-mongoose.connect(config.mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
 module.exports = mongoose;
