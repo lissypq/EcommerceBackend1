3320d4df67e4f945deea5750bb676dc7f8296fd0
lissypq <lissy0147@hotmail.com>
2025-06-19 00:12:46 -0400

    Update authRoutes.js

--- src/adapters/routes/authRoutes.js
+++ src/adapters/routes/authRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Router } = require('express');
 const AuthController = require('../controllers/AuthController');
  
@@ -10,17 +9,5 @@ module.exports = (signInUseCase) => {
   router.post('/signin', controller.signIn.bind(controller));
  
   return router;
-=======
-const { Router } = require('express');
-const AuthController = require('../controllers/AuthController');
- 
-module.exports = (signInUseCase) => {
-  const router = Router();
-  const controller = new AuthController(signInUseCase);
- 
-  // POST /api/v1/auth/signin
-  router.post('/signin', controller.signIn.bind(controller));
- 
-  return router;
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 };
