8897f3e07cde627f97a4c18f481aa108b25a9cab
lissypq <lissy0147@hotmail.com>
2025-06-19 00:25:06 -0400

    Update mysqlConnection.js

--- src/infraestructure/database/mysqlConnection.js
+++ src/infraestructure/database/mysqlConnection.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-const mysql = require('mysql2/promise');
-
-const pool = mysql.createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'usuario',
-  password: 'contraseña',
-  database: 'ecommerce',
-  waitForConnections: true,
-  charset: 'utf8mb4_general_ci',
-  connectTimeout: 10000,
-});
-
-pool
-  .getConnection()
-  .then(conn => {
-    console.log('✔️  Conexión a MySQL establecida');
-    conn.release();
-  })
-  .catch(err => {
-    console.error('✖️  Error al conectar a MySQL:', err.message);
-  });
-
-=======
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -46,5 +21,5 @@ pool
     console.error('✖️  Error al conectar a MySQL:', err.message);
   });
 
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = pool;
