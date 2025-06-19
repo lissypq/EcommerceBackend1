32d804ca3461171f15370ae24921b12b76f0dd04
lissypq <lissy0147@hotmail.com>
2025-06-19 00:27:20 -0400

    Update MongoProductRepository.js

--- src/infraestructure/repositories/MongoProductRepository.js
+++ src/infraestructure/repositories/MongoProductRepository.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-const ProductRepository = require('../../domain/repositories/ProductRepository');
-const ProductModel = require('../database/models/ProductModel');
-const Product = require('../../domain/entities/Product');
- 
-class MongoProductRepository extends ProductRepository {
-  async getAll() {
-    const products = await ProductModel.find();
-    return products.map(p => new Product(p.toObject()));
-  }
- 
-  async create(product) {
-    const newProduct = await ProductModel.create(product);
-    return new Product(newProduct.toObject());
-  }
-}
- 
-=======
 const ProductRepository = require('../../domain/repositories/ProductRepository');
 const ProductModel = require('../database/models/ProductModel');
 const Product = require('../../domain/entities/Product');
@@ -43,5 +25,5 @@ class MongoProductRepository extends ProductRepository {
     return product.toObject();
 }
  }
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = MongoProductRepository;
