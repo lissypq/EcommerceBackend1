b1702e7652b0774273dfd7aa918ff4b504ba33a2
lissypq <lissy0147@hotmail.com>
2025-06-19 00:27:37 -0400

    Update MongoUserRepository.js

--- src/infraestructure/repositories/MongoUserRepository.js
+++ src/infraestructure/repositories/MongoUserRepository.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const UserModel = require('../database/models/User');
  
 class MongoUserRepository {
@@ -19,26 +18,5 @@ class MongoUserRepository {
  
 }
  
-=======
-const UserModel = require('../database/models/User');
- 
-class MongoUserRepository {
-  async findByUsername(username) {
-    return UserModel.findOne({ username }).lean().exec();
-  }
- 
-   /**
-   * Crea y guarda un nuevo usuario en Mongo.
-   * @param {{ username: string, password: string, roles?: string[] }} userData
-   * @returns {Promise<Object>} documento creado en la base de datos
-   * @throws {Error} si ocurre un error al guardar el usuario
-   */
-  async create({ username, password, roles = ['user'] }) {
-    const user = new UserModel({ username, password, roles });
-    return user.save();
-  }
- 
-}
- 
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
 module.exports = MongoUserRepository;
