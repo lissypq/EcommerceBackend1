271d6f87c8f1307f6df2f79f818baca9db82b809
lissypq <lissy0147@hotmail.com>
2025-06-19 00:12:02 -0400

    Update UserController.js

--- src/adapters/controllers/UserController.js
+++ src/adapters/controllers/UserController.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-class UserController {
-  /**
-   * @param {import('../useCases/SignUp')} signUpUseCase
-   */
-  constructor(signUpUseCase) {
-    this.signUpUseCase = signUpUseCase;
-  }
- 
-  async signUp(req, res, next) {
-    try {
-      const dto = await this.signUpUseCase.execute(req.body);
-      res.status(201).json({ user: dto });
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  }
-}
- 
-module.exports = UserController;
-=======
 class UserController {
   /**
    * @param {import('../useCases/SignUp')} signUpUseCase
