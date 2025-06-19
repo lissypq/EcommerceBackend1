9715d20631c2843d15b084df7a1f727c1b35183b
lissypq <lissy0147@hotmail.com>
2025-06-19 00:10:13 -0400

    Update CartController.js

--- src/adapters/controllers/CartController.js
+++ src/adapters/controllers/CartController.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-const CreateCart = require('../../application/useCases/CreateCart');
-const CartDTO = require('../../application/dtos/CartDTO');
- 
-class CartController {
-  constructor(CartRepository) {
-    this.CreateCart = new CreateOrder(CartRepository);
-  }
- 
-  async create(req, res) {
-    try {
-      const cart = await this.CreateCart.execute(req.body);
-      res.status(201).json(new CartDTO(cart));
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  }
-
-
-
-  async getAll(req, res) {
-    try {
-      const carts = await this.CartRepository.getAll();
-      res.status(200).json(carts);
-    } catch (err) {
-      res.status(500).json({ message: 'Error retrieving carts' });
-    }
-  }
-}
- 
-module.exports = CartController;
-=======
 const CreateCart = require('../../application/useCases/CreateCart');
 const CartDTO = require('../../application/dtos/CartDTO');
 const CartModel = require('../../infraestructure/database/models/CartModel');
@@ -77,4 +45,4 @@ class CartController {
 }
  
 module.exports = CartController;
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
