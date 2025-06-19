b01e6130ee0a70d05fd5c450a2844d6cd44de0a2
lissypq <lissy0147@hotmail.com>
2025-06-19 00:11:17 -0400

    Update OrderDetailController.js

--- src/adapters/controllers/OrderDetailController.js
+++ src/adapters/controllers/OrderDetailController.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-const CreateOrderdetails = require('../../application/useCases/CreateOrderDetails');
-const OrderDetailsDTO = require('../../application/dtos/OrderDetailsDTO');
- 
-class OrderDetailController {
-  constructor(OrderDetailsRepository) {
-    this.CreateOrderdetails = new CreateOrderdetails(OrderDetailsRepository);
-  }
- 
-  async create(req, res) {
-    try {
-      const orderdetails = await this.CreateOrderdetails.execute(req.body);
-      res.status(201).json(new OrderDTO(orderdetails));
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  }
-
-
-
-  async getAll(req, res) {
-    try {
-      const ordersdetails = await this.OrderDetailsRepository.getAll();
-      res.status(200).json(ordersdetails);
-    } catch (err) {
-      res.status(500).json({ message: 'Error retrieving orders' });
-    }
-  }
-}
- 
-module.exports = OrderDetailController;
-=======
 const CreateOrderdetails = require('../../application/useCases/CreateOrderDetails');
 const OrderDetailsDTO = require('../../application/dtos/OrderDetailsDTO');
 const OrderDetailModel = require('../../infraestructure/database/models/OrderDetailModel');
@@ -63,4 +31,4 @@ class OrderDetailController {
 }
  
 module.exports = OrderDetailController;
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
+
