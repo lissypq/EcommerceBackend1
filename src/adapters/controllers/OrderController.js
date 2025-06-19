5bc6acc54bb65dd3e3c358e3902f2c0c79fa4689
lissypq <lissy0147@hotmail.com>
2025-06-19 00:10:54 -0400

    Update OrderController.js

--- src/adapters/controllers/OrderController.js
+++ src/adapters/controllers/OrderController.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-const CreateOrder = require('../../application/useCases/CreateOrder');
-const OrderDTO = require('../../application/dtos/OrderDTO');
- 
-class OrderController {
-  constructor(OrderRepository) {
-    this.CreateOrder = new CreateOrder(OrderRepository);
-  }
- 
-  async create(req, res) {
-    try {
-      const order = await this.CreateOrder.execute(req.body);
-      res.status(201).json(new OrderDTO(order));
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  }
-
-
-
-  async getAll(req, res) {
-    try {
-      const orders = await this.OrderRepository.getAll();
-      res.status(200).json(orders);
-    } catch (err) {
-      res.status(500).json({ message: 'Error retrieving orders' });
-    }
-  }
-}
- 
-module.exports = OrderController;
-=======
 const CreateOrder = require('../../application/useCases/CreateOrder');
 const OrderDTO = require('../../application/dtos/OrderDTO');
  
@@ -60,4 +28,3 @@ class OrderController {
 }
  
 module.exports = OrderController;
->>>>>>> 28f00e44f809fcb372e231256c57fde08f60c38e
